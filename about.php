67fa363ad76a814fa38c5f3954fbacc119c6eb84
Fabian <60275181+MCFabian@users.noreply.github.com>
2020-06-17 15:33:21 +0200

    Conncetion to dabase outsourced
    To make it easyer to commit changes into master, the connection.php file inlcudes all data for the database.
    
    IMPORANT: dont push from slave into master. First push it into refactoring-branch (without connection.php)

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
     <title>Über mich</title>
 </head>
 <?php include ('pages/head.php'); ?>
-<?php cookie(); ?> 
+<?php cookie(1); ?> 
         <div id="page-content">
             <div class="grid">
                 <div class="blocker">
--- pages/functions.php
+++ pages/functions.php
@@ -1,12 +1,5 @@
+<?php include ('conncetion.php'); ?>
 <?php 
-    //Verbindung mit Datenbanksystem herstellen	   
-    /*  USE FOR DOMAIN   $servername = "127.0.0.1:3306"; */
-    $servername = "localhost:3306";
-    $username = "Web";
-    $password = "W_9bdb23";
-    $database = "mainstage"; 
-    $connection = new PDO("mysql:host=$servername; dbname=$database; charset=utf8", $username, $password);
-    
     function getmenu() {
 			global $connection;
             //SQL-Zugriff auf Datensaetze
